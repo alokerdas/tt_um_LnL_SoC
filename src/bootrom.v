cc4e827c971de156057581bce78dca243781f591
alokerdas <aloke.das@ieee.org>
2024-08-13 13:39:03 +0530

    RAM ROM mixed

--- src/bootrom.v
+++ src/bootrom.v
@@ -12,23 +12,65 @@ module bootrom (
   output reg [15:0] dout
 );
 
-  reg [15:0] outbuf0;
-  reg [15:0] outbuf1;
-  reg [15:0] outbuf2;
-  reg [15:0] outbuf3;
-  reg [15:0] outbuf4;
-  reg [15:0] outbuf5;
-  reg [15:0] outbuf6;
-  reg [15:0] outbuf7;
-  reg [15:0] outbufE;
-  reg [15:0] outbufF;
+  reg [15:0] outbuf0, outbuf1, outbuf2, outbuf3, outbuf4, outbuf5, outbuf6, outbuf7;
+  reg [15:0] outbuf8, outbuf9, outbufA, outbufB, outbufC, outbufD, outbufE, outbufF;
   reg [15:0] dout_internal;
-  wire romclk, clk7th, clkEth, clkFth;
+  wire romclk, clk7th, clk8th, clk9th, clkAth, clkBth, clkCth, clkDth, clkEth, clkFth;
 
   assign romclk = clk & 1'b0;
   assign clk7th = clk & we & cs & (~addr[3] & addr[2] & addr[1] & addr[0]);
-  assign clkEth = clk & we & cs & (~addr[0] & addr[1] & addr[2] & addr[3]);
+  assign clk8th = clk & we & cs & (addr[3] & ~addr[2] & ~addr[1] & ~addr[0]);
+  assign clk9th = clk & we & cs & (addr[3] & ~addr[2] & ~addr[1] & addr[0]);
+  assign clkAth = clk & we & cs & (addr[3] & ~addr[2] & addr[1] & ~addr[0]);
+  assign clkBth = clk & we & cs & (addr[3] & ~addr[2] & addr[1] & addr[0]);
+  assign clkCth = clk & we & cs & (addr[3] & addr[2] & ~addr[1] & ~addr[0]);
+  assign clkDth = clk & we & cs & (addr[3] & addr[2] & ~addr[1] & addr[0]);
+  assign clkEth = clk & we & cs & (addr[3] & addr[2] & addr[1] & ~addr[0]);
   assign clkFth = clk & we & cs & (&addr);
+  
+  //RAM
+  always @ (posedge clk8th or posedge rst) begin
+    if (rst) begin
+      outbuf8 <= 16'h0000;
+    end else begin
+      outbuf8 <= din;
+    end
+  end
+  always @ (posedge clk9th or posedge rst) begin
+    if (rst) begin
+      outbuf9 <= 16'h0000;
+    end else begin
+      outbuf9 <= din;
+    end
+  end
+  always @ (posedge clkAth or posedge rst) begin
+    if (rst) begin
+      outbufA <= 16'h0000;
+    end else begin
+      outbufA <= din;
+    end
+  end
+  always @ (posedge clkBth or posedge rst) begin
+    if (rst) begin
+      outbufB <= 16'h0000;
+    end else begin
+      outbufB <= din;
+    end
+  end
+  always @ (posedge clkCth or posedge rst) begin
+    if (rst) begin
+      outbufC <= 16'h0000;
+    end else begin
+      outbufC <= din;
+    end
+  end
+  always @ (posedge clkDth or posedge rst) begin
+    if (rst) begin
+      outbufD <= 16'h0000;
+    end else begin
+      outbufD <= din;
+    end
+  end
   always @ (posedge clkEth or posedge rst) begin
     if (rst) begin
       outbufE <= 16'h0000;
@@ -43,6 +85,8 @@ module bootrom (
       outbufF <= din;
     end
   end
+  
+  // ROM
   always @ (posedge romclk or posedge rst) begin
     if (rst) begin
       outbuf0 <= 16'hF200;
@@ -94,7 +138,7 @@ module bootrom (
   end
   always @ (posedge clk7th or posedge rst) begin
     if (rst) begin
-      outbuf7 <= 16'h000F; // this is tricky, it tests bootrom, mem and spi
+      outbuf7 <= 16'h0008; // this is tricky, it tests bootrom, mem and spi
     end else begin
       outbuf7 <= din;
     end
@@ -110,6 +154,12 @@ module bootrom (
       'h5: dout_internal = outbuf5;
       'h6: dout_internal = outbuf6;
       'h7: dout_internal = outbuf7;
+      'h8: dout_internal = outbuf8;
+      'h9: dout_internal = outbuf9;
+      'hA: dout_internal = outbufA;
+      'hB: dout_internal = outbufB;
+      'hC: dout_internal = outbufC;
+      'hD: dout_internal = outbufD;
       'hE: dout_internal = outbufE;
       'hF: dout_internal = outbufF;
     endcase
