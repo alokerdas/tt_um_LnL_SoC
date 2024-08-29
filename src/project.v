e0161cc87081fbd763be5543ce5eacbf26405739
Aloke Das <aloke.das@ieee.org>
2024-08-29 17:49:42 +0530

    added timer IP

--- src/project.v
+++ src/project.v
@@ -22,16 +22,17 @@ module tt_um_LnL_SoC (
   wire [15:0] data_to_dev, data_to_cpu, boot_to_cpu;
   wire [11:0] addr_to_memio;
   wire [7:0] spi_to_cpu;
-  wire rw_to_mem, load_spi, unload_spi, en_to_spi, en_to_dev, en_to_boot;
+  wire rw_to_mem, load_spi, unload_spi, en_to_spi, en_to_dev, en_to_boot, en_to_timer;
 
-  assign uio_oe = 8'hF0; // Lower nibble all input, Upper all output
-  assign uio_out[3:0] = 4'h0; // uio_out unused bits
+  assign uio_oe = 8'hF8; // Lower nibble all input, Upper all output
+  assign uio_out[2:0] = 3'h0; // uio_out unused bits
 
   always @(posedge clk or negedge rst_n)
     if (~rst_n) rst_n_i <= 1'b0;
     else rst_n_i <= 1'b1;
 
-  assign en_to_spi = |addr_to_memio[11:5] & en_to_dev;
+  assign en_to_spi = |addr_to_memio[11:6] & en_to_dev;
+  assign en_to_timer = ~(|addr_to_memio[11:6]) & addr_to_memio[5] & en_to_dev;
   assign en_to_boot = ~(|addr_to_memio[11:5]) & en_to_dev;
   assign load_spi = rw_to_mem & en_to_spi;
   assign unload_spi = ~rw_to_mem & en_to_spi;
@@ -83,6 +84,17 @@ module tt_um_LnL_SoC (
     .mosi(uio_out[5]),
     .ssn_in(uio_in[2]),
     .ssn_out(uio_out[4])
+  );
+  timer T0 (
+`ifdef USE_POWER_PINS
+    .vccd1(plus),
+    .vssd1(minus),
+`endif
+    .rst(~rst_n_i),
+    .clkin(clk),
+    .cs(en_to_timer),
+    .divby(data_to_dev[2:0]),
+    .clkout(uio_out[3])
   );
 
   // avoid linter warning about unused pins:
